9b411d17d472c151819e3bd519e2b030b416849d
Rishikesh1001 <152897247+Rishikesh1001@users.noreply.github.com>
2026-07-15 12:54:34 +0530

    DB

--- src/main/java/com/rishikesh/demo/StudentServer/StudentController.java
+++ src/main/java/com/rishikesh/demo/StudentServer/StudentController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     StudentService studentService;
-
     @Autowired
+
+
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
